7b0feefb28f4ebac85c5e3ade8c92f2f158415b6
farid <farid.mouzoun@outlook.fr>
2023-12-18 10:33:35 +0100

    exo js
    exo conditions et opérateurs algorithmiques

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var jour1 = "lundi";
+var jour2 = "mardi";
+var jour3 = "mercredi";
+var jour4 = "jeudi";
+var jour5 = "vendredi";
+var jour6 = "samedi";
+var jour7 = "dimanche";
+
+var jour_user = parseInt(prompt("Quel jour de la semaine est-on?"));
+if (jour_user == jour1) {
+    alert("Le jour suivant sera " + jour2)
+} else if (jour_user == jour2) {
+    alert("Le jour suivant sera " + jour3)
+} 
